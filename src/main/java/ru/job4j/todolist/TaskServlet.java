ef8815fce427b6440ba4697b6d2d47f9e7d650e9
dmitrytishchenko <tdmitiu@mail.ru>
2020-06-27 19:00:45 +0300

    2. Лямбды и шаблон wrapper. [#217924]

--- src/main/java/ru/job4j/todolist/TaskServlet.java
+++ src/main/java/ru/job4j/todolist/TaskServlet.java
@@ -3,9 +3,11 @@ package ru.job4j.todolist;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,24 +17,17 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.function.Function;
 
 public class TaskServlet extends HttpServlet {
     private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+    private SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/plain");
         resp.setCharacterEncoding("UTF-8");
-        List<Task> result = new ArrayList<>();
-        try {
-            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-            Session session = sf.openSession();
-            session.beginTransaction();
-            result = session.createQuery("from ru.job4j.todolist.Task").list();
-            session.getTransaction().commit();
-            session.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        List result = this.tx(session -> session.createQuery("from ru.job4j.todolist.Task").list());
         ObjectMapper mapper = new ObjectMapper();
         String json = mapper.writeValueAsString(result);
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
@@ -43,17 +38,23 @@ public class TaskServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String desc = req.getParameter("desc");
-        try {
-            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-            Session session = sf.openSession();
-            session.beginTransaction();
-            Task task = new Task(desc, new Date(), false);
-            session.save(task);
-            session.getTransaction().commit();
-            session.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Task task = new Task(desc, new Date(), false);
+        this.tx(session -> session.save(task));
         resp.sendRedirect("/todo/index.html");
+    }
+
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 }
