af2985816688daeeff3384da9d92b13412ab3749
dmitrytishchenko <tdmitiu@mail.ru>
2020-08-22 21:47:32 +0300

    Реализовать площадку продаж машин. [#217934] ver2

---
+++ src/main/java/ru/job4j/service/DateServlet.java
@@ -0,0 +1,27 @@
+package ru.job4j.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("UTF-8");
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date();
+        String json = mapper.writeValueAsString(format.format(date));
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.write(json);
+        writer.flush();
+    }
+}
